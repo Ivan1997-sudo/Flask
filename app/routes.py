c7273ccfb4a46f0649f15322498c56d2d19ecc63
Ivan1997-sudo <vpochtennov@gmail.com>
2025-07-31 17:40:28 +0300

    Следующий шаг

--- app/routes.py
+++ app/routes.py
@@ -1,47 +1,37 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app import app
+import requests
 
 # Определяем маршрут и привязываем его к функции
+
 @app.route("/")
-def home():
-    return render_template("index.html")
+def form():
+    return render_template("form.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/submit", methods=["POST", "GET"])
+def submit():
+    if request.method == "POST":
+        name = request.form.get("name")  # Получаем имя из формы
+        email = request.form.get("email")  # Получаем email из формы
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+        color = request.form.get("color")
+        name_color = None
+        if color != "#000000":
+            url = f"https://api.color.pizza/v1/?values={color[1:]}"
+            response = requests.get(url)
+            if response.status_code == 200:
+                repos = response.json()
+                name_color = repos['paletteTitle']
+        else:
+            color = None
 
-
-
-
-@app.route('/hello')
-def hello():
-    return "Hello, world!"
-
-@app.route('/info')
-def info():
-    return "This is an informational page"
-
-@app.route('/calc/<x>/<y>')
-def calc(x, y):
-    try:
-        x = int(x)
-        y = int(y)
-    except ValueError:
-        return "Нужно указать числа"
-    return f"The sum of {x} and {y} is {int(x) + int(y)}"
-
-@app.route('/reverse/<text>')
-def reverse(text):
-    if len(text) > 1:
-        return text[::-1]
-    return "Текст должен быть длиннее"
-
-@app.route('/user/<name>/<age>')
-def user(name, age):
-    if int(age) >= 0:
-        return f"Hello, {name}. You are {age} years old."
-    return "Ваш возраст отрицателен, рекомендуем обратиться к врачу"
+        profession = request.form.get("profession")
+        hobbies = request.form.getlist("hobbies")
+        if len(hobbies) == 0:
+            hobbies = False
+        level = request.form.get("level")
+        thing = request.form.get("thing")
+        return render_template("result.html", name=name, email=email,
+        color=color, name_color = name_color, profession=profession, hobbies=hobbies, level=level, thing=thing)
+    else:
+        return redirect(url_for("form"))  # Если запрос GET, возвращаем на форму
